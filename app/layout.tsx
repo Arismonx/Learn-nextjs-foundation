5736acf9f9644a6ae48c78265c59a01608b95bb8
Arismonx <arismonx@gmail.com>
2025-02-15 16:59:39 +0700

    Images and Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google'
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+const roboto = Roboto({
+  weight: '500',
+  subsets: ['latin']
 });
 
 const geistMono = Geist_Mono({
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${roboto.className} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
+import ImagePublic from '@/public/next.svg'
+
 export default function HomePage(){
   return (
     <div>
       <h1>Hello Next.js!</h1>
-      
+      <br/>
+      <h2>Local Image</h2>
+      <Image 
+      src= {ImagePublic} 
+      alt="This image NEXT" 
+      width={200}
+      height={200}
+      />
+
+      <br />
+      <h1>Remote Image</h1>
+      <Image 
+      src="https://images.unsplash.com/photo-1738168279272-c08d6dd22002?q=80&w=928&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      alt="Picture of the author"
+      width={300}
+      height={0}
+      />
     </div>
   )
 }
