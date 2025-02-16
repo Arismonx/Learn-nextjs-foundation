8aafd4602c432a7e7227b6dc8f141b84ee07f21e
Arismonx <arismonx@gmail.com>
2025-02-16 17:06:24 +0700

    How to fetch data and stream

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,14 @@
-import styles from './Styles.module.css'
-//CSS Modules
-export default function Page(){
+export default async function Page() {
+
+    const data = await fetch('https://api.vercel.app/blog');
+    const posts = await data.json()
     return (
-        <h1 className={styles.blog}>
-           Hello Blog
-        </h1>
+        <ul>
+            {posts.map((post:any)=>(
+                <li key={post.id}>
+                    ID:{post.id}, {post.title}
+                </li>
+            ))}
+        </ul>
     )
-}
+  }
